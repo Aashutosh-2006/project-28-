217aecb81ca42b0961fa789dfe02be9ccc5cd8d5
Aashutosh-2006 <72963279+Aashutosh-2006@users.noreply.github.com>
2020-11-19 11:28:16 +0545

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,10 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render=Matter.Render;
+const Constraint=Matter.Constraint;
 var boy,boyImg;
+var launchForce=100;
 
 function preload()
 {
@@ -11,7 +14,7 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1300, 600);
 
 
 	engine = Engine.create();
@@ -20,15 +23,25 @@ function setup() {
 	//Create the Bodies Here.
 stoneObj=new stone(235,420,30);
 treeObj=new tree(600,350,50,50);
-mangoObj1=new mango
-mangoObj2=new mango
+mangoObj1=new mango(200,200,50,50)
+/*mangoObj2=new mango
 mangoObj3=new mango
 mangoObj4=new mango
 mangoObj5=new mango
-groundObj=new Ground(400,680,400,20);
+mangoObj6=new mango
+mangoObj7=new mango
+mangoObj8=new mango
+mangoObj9=new mango
+mangoObj10=new mango
+mangoObj11=new mango
+mangoObj12=new mango*/
+groundObj=new Ground(1050,580);
 boy=createSprite(200,400,50,100);
-boy.addImage("boyImg");
-slingshot = new SlingShot(stoneObj.body,{x:200,y:100});
+boy.addImage(boyImg);
+boy.scale=0.1;
+slingshotObj = new SlingShot(stoneObj.body,{x:200,y:100});
+
+var render = Render.create({ element: document.body, engine: engine, options: { width: 1300, height: 600, wireframes: false } });
 
 
 	Engine.run(engine);
@@ -42,19 +55,19 @@ function draw() {
   treeObj.display();
   stoneObj.display();
   mangoObj1.display();
-  mangoObj2.display();
+  /*mangoObj2.display();
   mangoObj3.display();
   mangoObj4.display();
-  mangoObj5.display();
+  mangoObj5.display();*/
   groundObj.display();
   boy.display();
-  slingshot.display();
+  slingshotObj.display();
 
   detectollision(stoneObj,mangoObj1);
-  detectollision(stoneObj,mangoObj2);
+  /*detectollision(stoneObj,mangoObj2);
   detectollision(stoneObj,mangoObj3);
   detectollision(stoneObj,mangoObj4);
-  detectollision(stoneObj,mangoObj5);
+  detectollision(stoneObj,mangoObj5);*/
   drawSprites();
  
 }
@@ -64,7 +77,7 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
-function detectollision(lstane,lmango){
+function detectollision(lstone,lmango){
 	mangoBodyPosition=lmango.body.setPosition
 	stoneBodyPosition=lstone.body.setPosition
 
@@ -77,7 +90,7 @@ function detectollision(lstane,lmango){
 function keyPressed(){
 	if(keyCode===32){
 		Matter.Body.setPosition(stoneObj.body,{x:235,y:420})
-		launcherObject.attach(stoneObj.body);
+		slingshotObj.attach(stoneObj.body);
 	}
 }
 

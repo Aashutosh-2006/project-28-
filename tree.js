ad99d9419a5ecbe8663de50523178b9192b26141
Aashutosh-2006 <72963279+Aashutosh-2006@users.noreply.github.com>
2020-11-19 11:29:51 +0545

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,23 +1,29 @@
 class tree{
-    constructor(x, y, width, height) {
-        var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
-        }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
+    constructor(x, y) {
+
+       
+        this.x=x;
+        this.y=y;
+        this.dustbinwidth = 450;
+        this.dustbinheight = 600;
+        this.wallThickness=10;
+
         this.image = loadImage("tree.png");
-        World.add(world, this.body);
+        this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true}) 
+        this.leftWallBody=Bodies.rectangle(0, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false}) 
+        this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:false}) 
+        World.add(world, this.bottomBody) 
+        World.add(world, this.leftWallBody)
+        World.add(world, this.rightWallBody);
+
       }
       display(){
-   
+        var posBottom=this.bottomBody.position;
         push();
-        translate(this.body.position.x, this.body.position.y);
-       
+        translate(posBottom.x, posBottom.y+10);  
+        fill(25);    
         imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+        image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
         pop();
       }
 }
